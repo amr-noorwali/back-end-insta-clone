f48fc276c2b11182b579a2083e59a51f9eeb7c4c
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-12 16:36:59 +0300

    timestamps column comment

--- database/migrations/2026_08_12_132934_create_comments_table.php
+++ database/migrations/2026_08_12_132934_create_comments_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->text('content');
+            $table->timestamps();
         });
     }
 
