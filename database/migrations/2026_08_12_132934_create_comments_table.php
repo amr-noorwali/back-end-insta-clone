159ee3d3bfaa61a7ca94a2c60cbb0eb42f40c0c7
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-12 16:35:42 +0300

    post forigon key in commentt

--- database/migrations/2026_08_12_132934_create_comments_table.php
+++ database/migrations/2026_08_12_132934_create_comments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
         });
     }
 
