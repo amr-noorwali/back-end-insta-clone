e1d2a8daf200f82d4e74a2e3408573706568abdf
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 23:52:38 +0300

    logout api

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+// auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
