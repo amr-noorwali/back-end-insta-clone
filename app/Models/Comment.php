6fa86c47eb992c9dc4c595833633116d548aa891
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 18:14:46 +0300

    create post method in comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,5 +12,10 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
     //
 }
