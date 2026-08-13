326826c0b9eaa5eeb79f1da231ec3f4740473339
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 18:56:38 +0300

    email in register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
         $validatedData = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255', 'unique:users,username'],
-            
+            'email' => ['required', 'email', 'unique:users,email'],
 
         ]);
     }
