223eed42f5653f9e322b55b5dda9d3612fe2787b
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 22:45:51 +0300

    email in  validatedData

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
 
     public function login(Request $request) {
         $validatedData = $request->validate([
-            
+            'email' => ['required', 'email'],
         ]);
     }
 
