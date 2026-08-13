22f998f847786c69b2776be0dc2e58c63a0a173c
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 15:17:37 +0300

    post forign key in likes

--- database/migrations/2026_08_13_121531_create_like_table.php
+++ database/migrations/2026_08_13_121531_create_like_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('like', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
