f3ed95018107a145ceabd3c6b4ea3dd0ba95f806
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 18:02:44 +0300

    create user function in post  model

--- app/Models/post.php
+++ app/Models/post.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class post extends Model
 {
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
     //
 }
