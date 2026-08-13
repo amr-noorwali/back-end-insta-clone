9983de07790d8afd16130deae25f0da52a01dc8a
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 15:33:09 +0300

    following forign ke from user in flollow table

--- database/migrations/2026_08_13_122808_create_follows_table.php
+++ database/migrations/2026_08_13_122808_create_follows_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
             $table->foreignId('follower_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('following_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
