f38eefa3c179df3ad306348df7b042bf4a91d292
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-14 01:39:31 +0300

    show post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,8 +42,11 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
+        $post->load('user:id,username');
+
+        return response()->json($post);
         //
     }
 
