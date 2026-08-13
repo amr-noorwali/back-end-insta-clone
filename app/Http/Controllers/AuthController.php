113b085c203e67ad67ec99d774d78d23913ce481
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 18:55:49 +0300

    name in register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public function register(Request $request) {}
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+
+        ]);
+    }
 
     public function login(Request $request) {}
 
