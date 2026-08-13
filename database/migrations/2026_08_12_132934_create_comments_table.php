30cfcc07b6ef934a6b5ba81a4429b602c2e1166a
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 15:24:30 +0300

    fixed doulicated tinstamp column in comment and migrated all

--- database/migrations/2026_08_12_132934_create_comments_table.php
+++ database/migrations/2026_08_12_132934_create_comments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->text('content');
