ed8fa5e66ae232ffbb5b71e22995c9975941415b
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-14 01:49:18 +0300

    import gate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
