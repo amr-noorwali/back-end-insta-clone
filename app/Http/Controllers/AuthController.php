7976f04c82825a0e9cf998044097e555cdf034fb
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-13 19:00:26 +0300

    username in register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
 
         $user = User::create([
             'name' => $validatedData['name'],
+            'username' => $validatedData['username'],
             
         ]);
     }
