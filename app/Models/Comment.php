7b1014612a89fa920b40cf2691521c93d8c4ffd1
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-14 18:58:48 +0300

    fillable in comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Comment extends Model
 {
 
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'content',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
