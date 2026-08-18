bd035c565db548a25b206c4644ac7400aaf4ca31
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-18 22:33:26 +0300

    return array in comment resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,6 +14,8 @@ class CommentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            
+        ];
     }
 }
