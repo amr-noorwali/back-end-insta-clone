c8d5f5234e98bd1f3eebf33a6439ee005a5ee83e
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-19 03:48:54 +0300

    fix: schema nake

--- database/migrations/2026_08_13_121531_create_like_table.php
+++ database/migrations/2026_08_13_121531_create_like_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('like', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
