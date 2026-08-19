019f3573f249692626995b8036916fd7a281eb99
Amr Noorwali <amr_a.h1424@hotmail.com>
2026-08-19 03:34:41 +0300

    import like controller in api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
